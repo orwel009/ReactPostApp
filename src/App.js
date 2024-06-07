c860f106c471b7277cab4b94a9d17702017c57de
orwel009 <orwelvincent1@gmail.com>
2024-06-07 21:48:18 +0530

    ISearch and Delete added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AddPost from './components/AddPost';
+import Search from './components/Search';
+import Delete from './components/Delete';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<AddPost/>}/>
+          <Route path='/search' element={<Search/>}/>
+          <Route path='/delete' element={<Delete/>}/>
         </Routes>
       </BrowserRouter>
     </div>
